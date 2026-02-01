6d270a3240197afc3edc7e50f6d7d4586edb6d5d
webstek <kyle.jr.webster@gmail.com>
2026-02-01 15:11:05 -0700

    chpc renders

--- lambda.bat
+++ lambda.bat
@@ -4,9 +4,9 @@
 #SBATCH --ntasks=1
 #SBATCH --partition=granite-guest
 #SBATCH --qos=granite-guest
-#SBATCH --account=cs6230
+#SBATCH --account=cs6969
 #SBATCH --cpus-per-task=96
-#SBATCH -t 0:02:00
+#SBATCH -t 0:20:00
 #SBATCH --export=SPP,SCATTERS,SAMPLEPROB
 
 # Usage: sbatch --export=SPP=1024,SCATTERS=64,SAMPLEPROB=0.9 lambda.bat
@@ -22,8 +22,7 @@ module load gcc/15.1.0
 echo "*** Compiling..." >> $LOGFILE
 make release
 
-ulimit -c unlimited
 cd bin
 echo "*** Rendering..." >> $LOGFILE
-(time ./lambda scenes/teapot.nls -s $SPP -b $SCATTERS -p $SAMPLEPROB) \
+(time ./lambda scenes/dispersion.nls -s $SPP -b $SCATTERS -p $SAMPLEPROB) \
   &>> $LOGFILE
